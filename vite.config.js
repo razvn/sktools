6068da4df4471ca5f8f993de57497ecc810a471e
Razvan <745873+razvn@users.noreply.github.com>
2023-01-16 15:57:23 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	base: ''
+	plugins: [sveltekit()]
 };
 
 export default config;
